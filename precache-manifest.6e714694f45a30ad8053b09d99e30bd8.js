2f34be7a6614ca73c8aed8849ca500afdf4ef8c1
benk1 <kakengi_bernard@hotmail.com>
2020-10-12 22:55:41 +0300

    Updates

---
+++ precache-manifest.6e714694f45a30ad8053b09d99e30bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdde1141bf1bca44524539da737bcd94",
+    "url": "/WorldCountries/index.html"
+  },
+  {
+    "revision": "0ef395d7d6b3b41beaf8",
+    "url": "/WorldCountries/static/css/main.8d2d54fa.chunk.css"
+  },
+  {
+    "revision": "73df19805fa746bd193a",
+    "url": "/WorldCountries/static/js/2.59efcfde.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/WorldCountries/static/js/2.59efcfde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef395d7d6b3b41beaf8",
+    "url": "/WorldCountries/static/js/main.fa23560c.chunk.js"
+  },
+  {
+    "revision": "ec851ab99f8c3cfee57c",
+    "url": "/WorldCountries/static/js/runtime-main.e16e916d.js"
+  }
+]);
